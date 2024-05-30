0e82e7455a56260d903a5afc1aa09318509ee387
Stefan Webster <swebster08@gmail.com>
2024-05-29 20:48:00 -0700

    Basic JSON working, removed virtual env

--- example/views.py
+++ example/views.py
@@ -7,16 +7,9 @@ import json
 
 def index(request):
     now = datetime.now()
-    # html = f'''
-    # <html>
-    #     <body>
-    #         <h1>Hello from Vercel!</h1>
-    #         <p>The current time is { now }.</p>
-    #     </body>
-    # </html>
-    # '''
     json_data = {
         "message": "Hello from Vercel!",
-        "time": now
+        "time": str(now)
     }
     return HttpResponse(json.dumps(json_data), content_type='application/json')
+

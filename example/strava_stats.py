a1fdead72566b992ddef13a0caf129ce0274bc51
greg <gregorybwest@gmail.com>
2024-09-08 15:45:49 -0700

    move all run calculations to backend

--- example/strava_stats.py
+++ example/strava_stats.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from dotenv import load_dotenv
+from services.calculators.running import calculate_average_run_time, calculate_average_run_pace, calculate_suggested_runs
 
 load_dotenv()
 
@@ -27,8 +28,13 @@ class StravaStatsView(APIView):
         print(headers)
         print(url)
         response = requests.get(url, headers=headers)
+        average_run_pace = calculate_average_run_pace(response.json())
+        average_run_time = calculate_average_run_time(response.json())
+        suggested_runs = calculate_suggested_runs(average_run_pace, average_run_time)
+        print(suggested_runs)
+
         print(response)
         if response.status_code != 200:
             return Response({"message": "Error from Strava API", "details": response.json()}, status=response.status_code)
         
-        return Response(response.json(), status=200)
+        return Response({"easy_run":suggested_runs[0], "medium_run":suggested_runs[1], "hard_run":suggested_runs[2]}, status=200)

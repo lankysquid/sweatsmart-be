848e60ac2928714e43d0cb0861e4df4e859003f8
Stefan Webster <swebster08@gmail.com>
2025-01-06 20:57:43 -0800

    Revert "Merge pull request #24 from lankysquid/no-pprint"
    This reverts commit 78e173391a7146e8fa2bd3183f2c97285ae59083, reversing
    changes made to f7567e2cef61656b0cd7d829c49e2957cad29dff.

--- example/strava_stats.py
+++ example/strava_stats.py
@@ -1,6 +1,6 @@
 import os
 import requests
-# from pprint import pprint
+from pprint import pprint
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
@@ -24,7 +24,7 @@ def get_stats(strava_stats: dict, strava_activities: dict) -> dict:
     kilojoules_array = [] 
     for activity in strava_activities[:10]:
         if activity.get('kilojoules'):
-            # pprint(activity["kilojoules"], indent=4)
+            pprint(activity["kilojoules"], indent=4)
             kilojoules_array.append(activity["kilojoules"])
     average_kjs = (sum(kilojoules_array) / len(kilojoules_array))
     recent_kjs = kilojoules_array[0]
@@ -74,6 +74,6 @@ class StravaStatsView(APIView):
         
         workouts = get_stats(strava_stats.json(), strava_activities.json())
         
-        # pprint(f'workouts {workouts}', indent=2)
+        pprint(f'workouts {workouts}', indent=2)
         
         return Response({"workouts": workouts}, status=200)

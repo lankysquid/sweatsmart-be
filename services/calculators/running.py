b60946ba36b0bfd033354fbbd6d0a9887004e75c
Stefan Webster <swebster08@gmail.com>
2025-02-26 21:44:02 -0800

    Tweaks for pace

--- services/calculators/running.py
+++ services/calculators/running.py
@@ -31,17 +31,17 @@ def calculate_runs(average_pace: float, average_time: float, suggested: str) -> 
         return {"To See Data, Record a Run in Strava"}
     easy_pace = average_pace_readable(average_pace * .95)
     easy_time = average_time * 0.8
-    easy_workout_plan = gpt_workout_details("Easy", "running", easy_time)
+    easy_workout_plan = gpt_workout_details("Easy", "running", easy_time, easy_pace, "min/mile")
     easy_title = easy_workout_plan.title
     easy_run = {"pace": easy_pace, "time": easy_time, "suggested": suggested == "EASY", "title": easy_title, "difficulty": "easy", "pace_unit": "mins/mi", "workout_plan": easy_workout_plan.plan} 
     medium_pace = average_pace_readable(average_pace * 0.825)
     medium_time = average_time * 0.9
-    medium_workout_plan = gpt_workout_details("Medium", "running", medium_time)
+    medium_workout_plan = gpt_workout_details("Medium", "running", medium_time, medium_pace, "min/mile")
     medium_title = medium_workout_plan.title
     medium_run = {"pace": medium_pace, "time": medium_time, "suggested": suggested == "MEDIUM", "title": medium_title, "difficulty": "medium", "pace_unit": "mins/mi", "workout_plan": medium_workout_plan.plan} 
     hard_pace = average_pace_readable(average_pace * 0.78)
     hard_time = average_time * 1.05
-    hard_workout_plan = gpt_workout_details("Difficult", "running", hard_time)
+    hard_workout_plan = gpt_workout_details("Difficult", "running", hard_time, hard_pace, "min/mile")
     hard_title = hard_workout_plan.title
     hard_run = {"pace": hard_pace, "time": hard_time, "suggested": suggested == "HARD", "title": hard_title, "difficulty": "hard", "pace_unit": "mins/mi", "workout_plan": hard_workout_plan.plan} 
     return [easy_run, medium_run, hard_run]

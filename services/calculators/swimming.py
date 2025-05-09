d56af58a218b5059936690cfe43a9f4c832449de
Stefan Webster <swebster08@gmail.com>
2025-05-08 20:40:53 -0700

    Update swimming.py

--- services/calculators/swimming.py
+++ services/calculators/swimming.py
@@ -1,3 +1,6 @@
+from services.calculators.running import average_pace_readable
+from services.gpt.chatbot import gpt_workout_details
+
 def calculate_average_swim_pace(strava_stats: dict) -> float:
     """Calculates average pace based on recent run totals."""
     elapsed_time = strava_stats["recent_swim_totals"]["elapsed_time"]
@@ -5,7 +8,7 @@ def calculate_average_swim_pace(strava_stats: dict) -> float:
     if elapsed_time == 0 or distance == 0:
         return 0
     # 1609.344 is used to convert meters to miles (for pace calculation)
-    average_pace = elapsed_time / (distance / 1609.344)
+    average_pace = elapsed_time / ((distance * 1.09361) / 100)
     return average_pace
 
 
@@ -22,16 +25,21 @@ def calculate_average_swim_time(strava_stats: dict) -> float:
 def calculate_swims(average_pace: float, average_time: float, suggested: str) -> dict:
     if average_time == 0:
         return {"Go for a swim to see data"}
-    easy_pace = average_pace * 1.05
+    easy_pace = average_pace_readable(average_pace * 1.05)
     easy_time = average_time * 0.8
-    easy_title = "Easy Workout"
-    easy_swim = {"pace": easy_pace, "time": easy_time, "suggested": suggested == "EASY", "title": easy_title, "difficulty": "easy"} 
-    medium_pace = average_pace * 0.95
+    easy_workout_plan = gpt_workout_details("Easy", "swimming", easy_time, easy_pace, "min/100yd")
+    easy_title = easy_workout_plan.title
+    easy_swim = {"pace": easy_pace, "time": easy_time, "suggested": suggested == "EASY", "title": easy_title, "difficulty": "easy", "pace_unit": "mins/100yd", "workout_plan": easy_workout_plan.plan} 
+   
+    medium_pace = average_pace_readable(average_pace * 0.95)
     medium_time = average_time * 0.92
-    medium_title = "Medium Workout"
-    medium_swim = {"pace": medium_pace, "time": medium_time, "suggested": suggested == "MEDIUM", "title": medium_title, "difficulty": "medium"} 
-    hard_pace = average_pace * 0.9
+    medium_workout_plan = gpt_workout_details("Medium", "swimming", medium_time, medium_pace, "min/100yd")
+    medium_title = medium_workout_plan.title
+    medium_swim = {"pace": medium_pace, "time": medium_time, "suggested": suggested == "MEDIUM", "title": medium_title, "difficulty": "medium", "pace_unit": "mins/100yd", "workout_plan": medium_workout_plan.plan} 
+    
+    hard_pace = average_pace_readable(average_pace * 0.9)
     hard_time = average_time * 1.05
-    hard_title = "Hard Workout"
-    hard_swim = {"pace": hard_pace, "time": hard_time, "suggested": suggested == "HARD", "title": hard_title, "difficulty": "hard"} 
+    hard_workout_plan = gpt_workout_details("Hard", "swimming", hard_time, hard_pace, "min/100yd")
+    hard_title = hard_workout_plan.title
+    hard_swim = {"pace": hard_pace, "time": hard_time, "suggested": suggested == "HARD", "title": hard_title, "difficulty": "hard", "pace_unit": "mins/100yd", "workout_plan": hard_workout_plan.plan} 
     return [easy_swim, medium_swim, hard_swim]
